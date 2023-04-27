7b014f33adf27b19add95e52cbf04781193ce735
unknown <glebkalinichenkolol@gmail.com>
2023-04-27 19:16:08 +0300

    first

---
+++ test.js
@@ -0,0 +1,25 @@
+fetch('https://jsonplaceholder.typicode.com/photos')
+
+  .then(response => response.json())
+
+  .then(data => {
+
+    const gallery = data.slice(0, 12);
+    const images = gallery.map(item => {
+        
+      const img = document.createElement('img');
+      img.src = item.url;
+      const div = document.createElement('div');
+      div.appendChild(img);
+      return div;
+
+    });
+
+    const container = document.createElement('div');
+    container.classList.add('image-grid');
+    container.append(...images);
+    document.body.appendChild(container);
+
+  })
+
+  .catch(error => console.error(error));
